b3162414c7b4e8cbc73090e3f727dc03e9e6fa20
Kilian sanchez <kilian0925@gmail.com>
2018-04-17 14:20:27 +0200

    test

---
+++ C-S_DE/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import * as firebase from 'firebase';
+
+import { HomePage } from '../pages/home/home';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+
+    var config = {
+      apiKey: "AIzaSyDkwmQeQXW38b3RjCkr3dVPgzgC07VJjqo",
+    authDomain: "d-s-de.firebaseapp.com",
+    databaseURL: "https://d-s-de.firebaseio.com",
+    projectId: "d-s-de",
+    storageBucket: "",
+    messagingSenderId: "259045726337"
+    };
+    firebase.initializeApp(config);
+    
+
+  }
+}
+
